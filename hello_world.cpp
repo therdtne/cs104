14f2b56f35e46110d6d23496d9fe588b98630b6e
Trent Herdtner <therdtne@usc.edu>
2019-01-08 22:18:56 -0800

    Hello_World

--- hello_world.cpp
+++ hello_world.cpp
@@ -8,6 +8,7 @@ int main()
 	std::cout << "Fight on!!!!!" << std::endl;
 	std::cout << "The wise man speaks because he has something to say," << std::endl;
 	std::cout << "...the fool speaks because he has to say something." << std::endl;
+	std::cout << " -Plato " << std::endl;
 		
 	
 }
